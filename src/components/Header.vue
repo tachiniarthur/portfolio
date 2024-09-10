e6420a9dd1974d4e3c08750e51fb092f90fc4b43
Arthur Tachini <arthur@facedigital.com.br>
2024-09-10 17:28:58 -0300

    Ajuste no header mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -21,7 +21,6 @@ const mobileMenuOpen = ref(false);
         <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div class="flex lg:flex-1">
                 <a href="#home" class="-m-1.5 p-1.5" aria-label="Redirect to home section">
-                    <span class="sr-only">Your Company</span>
                     <img class="h-16 w-32" src="../assets/my-profile-header.svg" alt="My photo profile" />
                 </a>
             </div>
@@ -39,8 +38,7 @@ const mobileMenuOpen = ref(false);
             <div class="fixed inset-0 z-50" />
             <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                 <div class="flex items-center justify-between">
-                    <a href="#home" class="-m-1.5 p-1.5" aria-label="Redirect to home section">
-                        <span class="sr-only">Your Company</span>
+                    <a href="#home" class="-m-1.5 p-1.5" aria-label="Redirect to home section" @click="mobileMenuOpen = false">
                         <img class="h-16 w-32" src="../assets/my-profile-header.svg" alt="My photo profile" />
                     </a>
                     <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false" aria-label="Close menu mobile" title="Close menu mobile">
